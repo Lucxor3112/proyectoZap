e53828c14af5aa18655f90081cd789b53df7b8d4
Lucas <lucascaliva3112@gmail.com>
2023-10-23 21:52:41 -0300

    Agregue cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
     <>
         <footer>
             <div className='footer'>
-                <h3 className='logo'>ZAP <FontAwesomeIcon icon={faUtensils} /></h3>
+                <h3 className='logo'>Z<FontAwesomeIcon icon={faBolt}/>P</h3>
                 <p className='copyright'>Copyright© 2023 ZAP, Inc</p>
                 <div className='detalle'>
                     <a href="#">Informacion legal</a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
     <div className='header'>
         <div className='logo'>
-            <h1>ZAP</h1>
+            <h1>Z<FontAwesomeIcon icon={faBolt}/>P</h1>
         </div>
         <div className='buy'>
           <a href="https://api.whatsapp.com/send?phone=543865442380&text=Quiero comprarte unas zapatillas que vi en la pagina!">  
